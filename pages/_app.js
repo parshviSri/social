1889c7062bcde7bce3b2f6ccd2ff70ebe7f7c206
parshviSri <parshvisri@gmail.com>
2022-12-03 10:03:24 +0530

    Adding images
    Signed-off-by: parshviSri <parshvisri@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import store from "./state/store";
+import { Provider } from "react-redux";
+import Navbar from "./components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp
